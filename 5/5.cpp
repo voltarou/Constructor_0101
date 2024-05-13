59ae817a8b677195851903e5640d81a029064895
voltarou <bagusadnan04@gmail.com>
2024-05-13 19:26:02 +0800

    destruktor

---
+++ 5/5.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class angka {
+private :
+    int* arr;
+    int panjang;
+public:
+    angka(int);
+    ~angka();
+    void cetakData();
+    void isiData();
+};
+
+angka::angka(int i) {
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka() {
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::cetakData() {
+    for(int i = 1; i <= panjang; i++) {
+        cout << i << "=" << arr[i] << endl;
+    }
+}
+
+void angka::isiData() {
+    for (int i = 1; i <= panjang; i++) {
+        cout << i << "="; cin >> arr[i];
+    }
+    cout << endl;
+}
+int main()
+{
+    angka belajarcpp(3);
+    angka* ptrBelajarcpp = new angka(5);
+    delete ptrBelajarcpp;
+
+    return 0;
+}
+
+
