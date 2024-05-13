0fcb4fe7e26d746354b405ed3467b259f036ef2a
voltarou <bagusadnan04@gmail.com>
2024-05-13 11:30:36 +0800

    void setid

--- BaratsKecil/BaratsKecil.cpp
+++ BaratsKecil/BaratsKecil.cpp
@@ -8,6 +8,14 @@ public:
     static int nim;
     int id;
     string nama;
+
+    void setID();
+    void printAll();
+    mahasiswa(string pnama)
+    {
+        nama = pnama;
+        setID();
+    }
 };
 
 int main()
