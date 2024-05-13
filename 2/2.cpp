782f15da7be73d31db9d07dde52e41d78608d08b
voltarou <bagusadnan04@gmail.com>
2024-05-13 11:55:30 +0800

    class mahasiswa2

--- 2/2.cpp
+++ 2/2.cpp
@@ -2,6 +2,24 @@
 #include <string>
 using namespace std;
 
+class mahasiswa
+{
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; }
+    static int getNim() { return nim; }
+
+    mahasiswa(string pnama) : nama(pnama) { setID(); }
+
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
